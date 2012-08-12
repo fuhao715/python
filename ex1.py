576982fff99743de98e7164082f4dba693c32b14
fuhao715 <fuhao-715@163.com>
2012-08-12 16:01:07 +0800

    新增打印语句

---
+++ ex1.py
@@ -0,0 +1,8 @@
+﻿# -*- coding: utf-8 -*-
+print "hello world !"
+print "Hello Again"
+print "I like typing this."
+print "This is fun"
+print 'Yay ! Printing'
+print "I'd much rather you 'not' ."
+print 'I "said" do not touch this. w '
