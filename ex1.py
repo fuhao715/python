bf242e8faedb74bdb2d160a56256f1ac331cdff6
fuhao715 <fuhao-715@163.com>
2012-09-15 23:23:08 +0800

    修改byT430

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 print "hello world !"
 print "Hello Again"
 print "I like typing this."
@@ -6,3 +6,4 @@ print "This is fun"
 print 'Yay ! Printing'
 print "I'd much rather you 'not' ."
 print 'I "said" do not touch this. w 中国文字'
+print 'thinkpad T430'
