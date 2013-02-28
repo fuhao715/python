c546c57a5b0a51aa9d1263d47e210ad666d837b9
fuhao715 <fuhao715@gmail.com>
2013-02-28 16:32:45 +0800

    break 、continue

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -52,3 +52,22 @@ for i in range(0,9):
     print i
 else:
     print 'Done'
+
+
+# break 、continue
+while True:
+    s = raw_input('Enter something : ')
+    if s == 'quit':
+        break
+    print 'Length of the string is', len(s)
+print 'Done'
+
+
+while True:
+    s = raw_input('Enter something : ')
+    if s == 'quit':
+        break
+    if len(s) < 3:
+        continue
+    print 'Input is of sufficient length'
+    # Do other kinds of processing here...
