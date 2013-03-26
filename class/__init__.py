ed946fc0d59a06a322ed6f49d900bc0802534ac7
fuhao715 <fuhao-715@163.com>
2013-03-26 19:12:55 +0800

    面向对象学习第一课

---
+++ class/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+__author__ = 'fuhao'
+'''
+  PROJECT_NAME:python
+  PACKAGE_NAME:
+  File_Name:__init__.py.py
+  User: fuhao
+  Date: 13-3-26 : 下午4:43
+  descripton:
+'''
---
+++ class/simplestclass.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+__author__ = 'fuhao'
+'''
+  PROJECT_NAME:python
+  PACKAGE_NAME:
+  File_Name:simplestclass.py
+  User: fuhao
+  Date: 13-3-26 : 下午4:43
+  descripton:
+'''
+class Person:
+   def sayHI(self,name):
+       print 'hello ,',name
+
+p = Person()
+p.sayHI('python')
