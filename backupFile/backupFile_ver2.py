7ce1d2c2f577a01c23b5e253552dbe5865b6e3fe
fuhao715 <fuhao-715@163.com>
2013-03-26 16:36:00 +0800

    备份文件第二版

---
+++ backupFile/backupFile_ver2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+__author__ = 'fuhao'
+'''
+  PROJECT_NAME:python
+  PACKAGE_NAME:
+  File_Name:backupFile_ver2.py
+  User: fuhao
+  Date: 13-3-26 : 下午2:26
+  descripton:
+'''
+import os,time
+
+# 1. The files and directories to be backed up are specified in a list.
+source = ['/home/swaroop/byte', '/home/swaroop/bin']
+# If you are using Windows, use source = [r'C:\Documents', r'D:\Work'] or something like that
+
+# 2. The backup must be stored in a main backup directory
+target_dir = '/mnt/e/backup/' # Remember to change this to what you will be using
+
+# 3. The files are backed up into a zip file.
+# 4. The current day is the name of the subdirectory in the main directory
+today = target_dir + time.strftime('%Y%m%d')
+# The current time is the name of the zip archive
+now = time.strftime('%H%M%S')
+# Take a comment from the user to create the name of the zip file
+comment = raw_input('Enter a comment --> ')
+if len(comment) == 0: # check if a comment was entered
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' + \
+             comment.replace(' ', '_') + '.zip'
+# Notice the backslash!
+# Create the subdirectory if it isn't already there
+if not os.path.exists(today):
+    os.mkdir(today) # 创建文件目录
+    print 'Successfully created directory', today
+# The name of the zip file
+target = today + os.sep + now + '.zip'
+
+# 5. We use the zip command (in Unix/Linux) to put the files in a zip archive
+zip_command = "zip -qr '%s' %s" % (target, ' '.join(source))
+
+# Run the backup
+if os.system(zip_command) == 0:
+    print 'Successful backup to', target
+else:
+    print 'Backup FAILED'
