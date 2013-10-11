c481b6f2796e425b381cd9262bf8f22999754daa
fuhao715 <fuhao715@gmail.com>
2013-10-11 16:11:15 +0800

    方法多返回值

--- fileUtils/readFile.py
+++ fileUtils/readFile.py
@@ -12,11 +12,20 @@ txt = open(filename)
 
 print "read 读取文件名为：%r:" % filename
 print txt.read()
-
+txt.close()
 print "再读取一遍文件: "
 file_again = raw_input(">")
 
 txt_again = open(file_again)
 print txt_again.read()
-
+txt_again.close()
 print 'done'
+
+
+def print_more(*args):
+    first, second, third = args
+    print '%s,%s,%s' % (first, second, third)
+    return first, second, third
+
+first_str, second_str, third_str = print_more('f', 'se', 'th')
+print 'print again %s,%s,%s' % (first_str, second_str, third_str)
