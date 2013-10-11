5e78179c4ea34263c467e522a54e4dbe31b789d2
fuhao715 <fuhao715@gmail.com>
2013-10-11 17:06:56 +0800

    类方法添加参数

--- class/simplestclass.py
+++ class/simplestclass.py
@@ -9,11 +9,18 @@ __author__ = 'fuhao'
   Date: 13-3-26 : 下午4:43
   descripton:
 '''
+
+
 class Person:
-    def __init__(self,name):
-        self.name=name
-    def sayHI(self):
-       print 'hello  ,',self.name
+    def __init__(self, name):
+        self.name = name
+
+    def say_hi(self):
+        print 'hello ,', self.name
+
+    def say_bye(self, names):
+        print 'bye:', names
 
 p = Person('pythoner')
-p.sayHI()
+p.say_hi()
+p.say_bye('hello')
