e195db25af7f5f977fc26416f10ef59eceae8c30
fuhao715 <fuhao715@gmail.com>
2013-10-12 13:35:06 +0800

    猜数游戏，熟悉类的使用

---
+++ game/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 13-10-12
+# * Time: 上午10:38
+# * To change this template use File | Settings | File Templates.
