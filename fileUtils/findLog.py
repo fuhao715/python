9e7550de12cfca60525a69b528f1b3af328d0776
fuhao715 <fuhao715@gmail.com>
2013-10-24 18:03:35 +0800

    在文件目录中查找日志文件中制定内容

---
+++ fileUtils/findLog.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 13-10-24
+# * Time: 下午3:23
+# * To change this template use File | Settings | File Templates.
+import os
+current_path = os.getcwd()
+#print current_path + '\n'
+
+
+def wald_dir(dir, fileinfor, topdown=True):
+    for root, dirs, files in os.walk(dir, topdown):
+        for name in files:
+            #print os.path.join(root, name)
+            find_information(os.path.join(root, name), fileinfor)
+        for name in dirs:
+            print os.path.join(root, name)
+
+
+def find_information(log_file, infor):
+    txt = open(log_file, 'r')
+    for (line, value) in enumerate(txt):
+        index = value.find(infor)
+        if index != -1:
+            print '%s  %d  %s \n' % (log_file[log_file.find('domains/')+8:], line, value)
+    txt.close()
+
+
+print current_path + '/logs'
+infor = raw_input('请输入要查找的字符串:\n')
+wald_dir(current_path + '/logs', infor)
+
