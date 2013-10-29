f9199326c695eabf8a36b2cfc2394f099384c894
fuhao715 <fuhao715@gmail.com>
2013-10-29 14:44:09 +0800

    getattr() 第三个参数为默认值，若未输入第三个参数则返回默认值,getattr有点类似java反射的用法

---
+++ game/compute.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 13-10-29
+# * Time: 上午10:53
+# * To change this template use File | Settings | File Templates.
+
+
+def plus(a, b):
+    return a+b
+
+
+def subtract(a, b):
+    return a-b
+
+
+def multiply(a, b):
+    return a*b
+
+
+def divide(a, b):
+    return a/b
