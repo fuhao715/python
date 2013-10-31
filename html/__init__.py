30b33dc244f89b648fa128f1ff48886882b2d6ff
fuhao715 <fuhao715@gmail.com>
2013-10-31 16:24:51 +0800

    html文件解析

---
+++ html/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 13-10-31
+# * Time: 下午3:35
+# * To change this template use File | Settings | File Templates.
