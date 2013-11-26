b680dde7b916d794f2a740aa277eaeb2c74df7f3
fuhao715 <fuhao715@gmail.com>
2013-11-26 08:58:33 +0800

    Pillow图像处理，学习网址

--- image/ImageEx1.py
+++ image/ImageEx1.py
@@ -5,6 +5,7 @@ __author__ = 'fuhao'
 # * Date: 13-11-26
 # * Time: 上午8:43
 # * To change this template use File | Settings | File Templates.
+# 学习网址 http://pillow.readthedocs.org/en/latest/handbook/tutorial.html
 from PIL import Image
 
 im = Image.open("D:/css123.jpg")
