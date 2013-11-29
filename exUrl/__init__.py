64a3594beb210384fb84b610bf38578e1e9bdfbd
fuhao715 <fuhao715@gmail.com>
2013-11-29 17:11:12 +0800

    url解析

---
+++ exUrl/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 13-11-29
+# * Time: 下午4:55
+# * To change this template use File | Settings | File Templates.
