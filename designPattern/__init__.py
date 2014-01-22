3842997fe6dbbe7fedc243b231d36c1479d9b60b
fuhao715 <fuhao715@gmail.com>
2014-01-22 16:10:19 +0800

    适配器模式

---
+++ designPattern/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 14-1-22
+# * Time: 上午9:21
+# * To change this template use File | Settings | File Templates.
---
+++ designPattern/generic_adapter.py
@@ -0,0 +1,80 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 14-1-22
+# * Time: 下午3:19
+# * To change this template use File | Settings | File Templates.
+import os
+
+
+class Dog(object):
+    def __init__(self):
+        self.name = "Dog"
+
+    def bark(self):
+        return "woof!"
+
+
+class Cat(object):
+    def __init__(self):
+        self.name = "Cat"
+
+    def meow(self):
+        return "meow!"
+
+
+class Human(object):
+    def __init__(self):
+        self.name = "Human"
+
+    def speak(self):
+        return "'hello'"
+
+
+class Car(object):
+    def __init__(self):
+        self.name = "Car"
+
+    def make_noise(self, octane_level):
+        return "vroom%s" % ("!" * octane_level)
+
+
+class Adapter(object):
+    """
+    Adapts an object by replacing methods.
+    Usage:
+    dog = Dog
+    dog = Adapter(dog, dict(make_noise=dog.bark))
+    """
+    def __init__(self, obj, adapted_methods):
+        """We set the adapted methods in the object's dict"""
+        self.obj = obj
+        self.__dict__.update(adapted_methods)
+
+    def __getattr__(self, attr):
+        """All non-adapted calls are passed to the object"""
+        return getattr(self.obj, attr)
+
+
+def test():
+    objects = []
+    dog = Dog()
+    objects.append(Adapter(dog, dict(make_noise=dog.bark)))
+    cat = Cat()
+    objects.append(Adapter(cat, dict(make_noise=cat.meow)))
+    human = Human()
+    objects.append(Adapter(human, dict(make_noise=human.speak)))
+    car = Car()
+    car_noise = lambda: car.make_noise(3)
+    objects.append(Adapter(car, dict(make_noise=car_noise)))
+
+    for obj in objects:
+        print("A", obj.name, "goes", obj.make_noise())
+
+
+if __name__ == "__main__":
+    test()
+    adog = Dog()
+    adapter = Adapter(adog, dict(make_noise=adog.bark))
+    print(adapter.name,"-"*20,adapter.make_noise())
