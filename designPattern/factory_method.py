c1b72cfa0ecc12fa09f8a2fe777d53f24a8a5b6d
fuhao715 <fuhao715@gmail.com>
2014-01-23 15:21:12 +0800

    工厂模式 factory_method

---
+++ designPattern/factory_method.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 14-1-23
+# * Time: 下午3:20
+# * To change this template use File | Settings | File Templates.
+
+class GreekGetter:
+    """A simple localizer a la gettext"""
+    def __init__(self):
+        self.trans = dict(dog="σκύλος", cat="γάτα")
+
+    def get(self, msgid):
+        """We'll punt if we don't have a translation"""
+        try:
+            return self.trans[msgid]
+        except KeyError:
+            return str(msgid)
+
+
+class EnglishGetter:
+    """Simply echoes the msg ids"""
+    def get(self, msgid):
+        return str(msgid)
+
+
+def get_localizer(language="English"):
+    """The factory method"""
+    languages = dict(English=EnglishGetter, Greek=GreekGetter)
+    return languages[language]()
+
+# Create our localizers
+e, g = get_localizer("English"), get_localizer("Greek")
+# Localize some text
+for msgid in "dog parrot cat bear".split():
+    print(e.get(msgid), g.get(msgid))
