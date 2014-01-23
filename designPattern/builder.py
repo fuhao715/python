ead89b43fd8659801266dc8f6020d8e270030e89
fuhao715 <fuhao715@gmail.com>
2014-01-23 15:10:16 +0800

    建造者模式 builder

---
+++ designPattern/builder.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 14-1-23
+# * Time: 下午3:07
+# * To change this template use File | Settings | File Templates.
+# Director
+class Director(object):
+    def __init__(self):
+        self.builder = None
+
+    def construct_building(self):
+        self.builder.new_building()
+        self.builder.build_floor()
+        self.builder.build_size()
+
+    def get_building(self):
+        return self.builder.building
+
+
+# Abstract Builder
+class Builder(object):
+    def __init__(self):
+        self.building = None
+
+    def new_building(self):
+        self.building = Building()
+
+
+# Concrete Builder
+class BuilderHouse(Builder):
+    def build_floor(self):
+        self.building.floor = 'One'
+
+    def build_size(self):
+        self.building.size = 'Big'
+
+
+class BuilderFlat(Builder):
+    def build_floor(self):
+        self.building.floor = 'More than One'
+
+    def build_size(self):
+        self.building.size = 'Small'
+
+
+# Product
+class Building(object):
+    def __init__(self):
+        self.floor = None
+        self.size = None
+
+    def __repr__(self):
+        return 'Floor: %s | Size: %s' % (self.floor, self.size)
+
+
+# Client
+if __name__ == "__main__":
+    director = Director()
+    director.builder = BuilderHouse()
+    director.construct_building()
+    building = director.get_building()
+    print(building)
+    director.builder = BuilderFlat()
+    director.construct_building()
+    building = director.get_building()
+    print(building)
