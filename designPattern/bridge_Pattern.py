54626cc3afa2ac29f6076e891af99966603ada7e
fuhao715 <fuhao715@gmail.com>
2014-01-23 15:06:56 +0800

    桥接模式 bridge

---
+++ designPattern/bridge_Pattern.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 14-1-23
+# * Time: 下午3:04
+# * To change this template use File | Settings | File Templates.
+# ConcreteImplementor 1/2
+class DrawingAPI1(object):
+    def draw_circle(self, x, y, radius):
+        print('API1.circle at {}:{} radius {}'.format(x, y, radius))
+
+
+# ConcreteImplementor 2/2
+class DrawingAPI2(object):
+    def draw_circle(self, x, y, radius):
+        print('API2.circle at {}:{} radius {}'.format(x, y, radius))
+
+
+# Refined Abstraction
+class CircleShape(object):
+    def __init__(self, x, y, radius, drawing_api):
+        self._x = x
+        self._y = y
+        self._radius = radius
+        self._drawing_api = drawing_api
+
+    # low-level i.e. Implementation specific
+    def draw(self):
+        self._drawing_api.draw_circle(self._x, self._y, self._radius)
+
+    # high-level i.e. Abstraction specific
+    def scale(self, pct):
+        self._radius *= pct
+
+
+def main():
+    shapes = (
+        CircleShape(1, 2, 3, DrawingAPI1()),
+        CircleShape(5, 7, 11, DrawingAPI2())
+    )
+
+    for shape in shapes:
+        shape.scale(2.5)
+        shape.draw()
+
+
+if __name__ == '__main__':
+    main()
