baa49aa15839985614c9e02a30a9479d75348b1a
fuhao715 <fuhao715@gmail.com>
2014-01-23 15:37:55 +0800

    原型模式 prototype

---
+++ designPattern/prototype.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'fuhao'
+# * Created by PyCharm.
+# * User: fuhao
+# * Date: 14-1-23
+# * Time: 下午3:37
+# * To change this template use File | Settings | File Templates.
+import copy
+
+
+class Prototype:
+    def __init__(self):
+        self._objects = {}
+
+    def register_object(self, name, obj):
+        """Register an object"""
+        self._objects[name] = obj
+
+    def unregister_object(self, name):
+        """Unregister an object"""
+        del self._objects[name]
+
+    def clone(self, name, **attr):
+        """Clone a registered object and update inner attributes dictionary"""
+        obj = copy.deepcopy(self._objects.get(name))
+        obj.__dict__.update(attr)
+        return obj
+
+
+def main():
+    class A:
+        pass
+
+    a = A()
+    prototype = Prototype()
+    prototype.register_object('a', a)
+    b = prototype.clone('a', a=1, b=2, c=3)
+
+    print(a)
+    print(b.a, b.b, b.c)
+
+
+if __name__ == '__main__':
+    main()
+
