1497ecbdaca3547c06b25967f055565ee0feec09
fuhao715 <fuhao715@gmail.com>
2014-01-24 11:27:55 +0800

    logger 使用

--- ex6/func_doc.py
+++ ex6/func_doc.py
@@ -6,6 +6,8 @@ __author__ = 'fuhao'
 '''
 #!/usr/bin/python
 # Filename: func_doc.py
+import logging
+import os
 
 def printMax(x, y):
     '''Prints the maximum of two numbers.
@@ -22,3 +24,9 @@ def printMax(x, y):
 printMax(3, 5)
 print printMax.__doc__
 
+logging.basicConfig(filename=os.path.join(os.getcwd(), 'log.txt'), level=logging.WARN, format='%(asctime)s - %(levelname)s: %(message)s')
+logging.debug('this is a debug message')
+logging.debug('debug')  #被忽略
+logging.info('info')    #被忽略
+logging.warning('warn')
+logging.error('error')
