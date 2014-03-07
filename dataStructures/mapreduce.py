8e3a50ddac25581e35cdcb12560180fcb9bc8701
fuhao715 <fuhao715@gmail.com>
2014-03-07 10:44:24 +0800

    匿名函数，函数返回函数，函数参数为函数，可变参数

--- dataStructures/mapreduce.py
+++ dataStructures/mapreduce.py
@@ -46,12 +46,52 @@ def str2int(s):
 
 print str2int('13579')
 
+# python中除了list、dict是对象类型，在函数中是传递引用地址，其余的全部是传值，复制副本传递给函数。
 def test1(x):
     print x, id(x)
     x = x.upper()
     print x, id(x)
+    return x
 
 a = 'a'
 print a, id(a)
 test1(a)
-print a,id(a)
+print a, id(a)
+
+
+# list ['a','b',2,4,'abc','efg']
+# set和dict类似，也是一组key的集合，但不存储value。由于key不能重复.set([1, 2, 3])
+# *args是可变参数，args接收的是一个tuple；(1,2,3)
+#**kw是关键字参数，kw接收的是一个dict。{'a': 1, 'b': 2}
+# python高阶函数之函数传递、返回
+def calc_sum(*args):
+    ax = 0
+    for n in args:
+        ax = ax +n
+    return ax
+
+
+def lazy_sum(*args):
+    def sum_later():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum_later
+
+print calc_sum(1, 3, 5, 7, 9)
+print lazy_sum(1, 3, 5, 7, 9)
+print lazy_sum(1, 3, 5, 7, 9)()
+
+#匿名函数
+complex_num = map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print complex_num
+f = lambda x: x + 10
+print f(2)
+
+
+def build(x, y):
+    return lambda: x * x + y * y
+
+fun = build(3, 6)
+print fun, fun()
