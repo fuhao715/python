068f9c56ec29d575fa73c47f55e90fa22e868dcd
fuhao715 <fuhao715@gmail.com>
2014-03-18 16:37:21 +0800

    读取中文文件注意编码方式

--- backupFile/reNameFile.py
+++ backupFile/reNameFile.py
@@ -1,8 +1,19 @@
+# -*- coding: UTF-8 -*-
 __author__ = 'fuhao'
+
 import os
-path = 'C:\\Users\\fuhao\\Desktop\\temp'
-for file in os.listdir(path):
-    if os.path.isfile(os.path.join(path,file))==True:
-        newname = file.replace("tmp", "jpg")
-        os.rename(os.path.join(path, file), os.path.join(path, newname))
-        print(file)
+
+
+def rename_files():
+    path = 'C:\\Users\\fuhao\\Desktop\\temp'
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path,file))==True:
+            newname = file.replace("tmp", "jpg")
+            os.rename(os.path.join(path, file), os.path.join(path, newname))
+            print(file)
+
+
+if __name__ == '__main__':
+    file_name= u'C:/Users/fuhao/Desktop/golang升级更新方法.txt'
+    f = open(file_name, 'r')
+    print f.read()
