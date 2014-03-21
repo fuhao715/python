a5aa7b503e9b8325d66734b3f9c0c63bb6f1e0db
fuhao715 <fuhao715@gmail.com>
2014-03-21 10:26:39 +0800

    嵌套decorator使用

--- dataStructures/decoratorEx.py
+++ dataStructures/decoratorEx.py
@@ -67,3 +67,22 @@ def func_sqrt(x, y):
 func_sqrt(x=2, y=3, variable='variable')
 func_sqrt(x=4, y=5)
 
+def makeHtmlTag(tag, *args, **kwds):
+    def real_decorator(fn):
+        css_class = " class='{0}'".format(kwds["css_class"]) \
+                                     if "css_class" in kwds else ""
+        def wrapped(*args, **kwds):
+            return "<"+tag+css_class+">" + fn(*args, **kwds) + "</"+tag+">"
+        return wrapped
+    return real_decorator
+
+@makeHtmlTag(tag="b", css_class="bold_css")
+@makeHtmlTag(tag="i", css_class="italic_css")
+def hello():
+    return "hello world"
+
+print hello()
+
+# 输出：
+# <b class='bold_css'><i class='italic_css'>hello world</i></b>
+
