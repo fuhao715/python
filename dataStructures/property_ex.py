f8c1066cac3f07c7378dfe49c923d9a53ec0dfb3
fuhao715 <fuhao-715@163.com>
2014-05-05 16:19:11 +0800

    定制类 __str__ __repr__ __iter__

--- dataStructures/property_ex.py
+++ dataStructures/property_ex.py
@@ -21,7 +21,40 @@ class Student(object):
          else:
              self._score = value
 
+    def __init__(self, name):
+        self.name = name
 
-st = Student()
+    def __str__(self):
+        return 'Student object (name: %s)' % self.name
+
+    __repr__ = __str__
+
+
+
+
+
+st = Student('michael')
 st.score = 70
 print st.score
+print Student('Amy')
+print st
+
+
+
+# __iter__
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1   # 初始化两个计数器a，b
+
+    def __iter__(self):
+        return self     # 实例本身就是迭代对象，故返回自己
+
+    def next(self):
+        self.a, self.b = self.b, self.a + self.b    # 计算下一个值
+        if self.a > 100000:     # 退出循环的条件
+            raise StopIteration();
+        return self.a   # 返回下一个值
+
+
+for n in Fib():
+    print n
