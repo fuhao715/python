0e78c118a4aaec4959f1746da46ec9d2f9a2621b
fuhao715 <fuhao-715@163.com>
2014-05-09 10:37:54 +0800

    读取文件

---
+++ fileUtils/readChineseFile.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = 'fuhao'
+'''
+ * Created with PyCharm.
+ * User: fuhao
+ * Date: 14-5-8
+ * Time: 下午8:53
+ * To change this template use File | Settings | File Templates.
+'''
+with open('D:/test.md', 'rb') as f:
+    u = f.read()# .decode('gbk')
+    print u
