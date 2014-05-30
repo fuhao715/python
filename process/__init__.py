0f458ef9b6ca309f63b082c93632ded5dba637fa
fuhao715 <fuhao-715@163.com>
2014-05-30 17:36:10 +0800

    启动一个子进程并等待其结

---
+++ process/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'fuhao'
+'''
+ * Created with PyCharm.
+ * User: fuhao
+ * Date: 2014/5/30
+ * Time: 17:30
+ * To change this template use File | Settings | File Templates.
+'''
